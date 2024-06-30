d023416900bdad257a63e7301eabeb5743bcee84
Matt Pfeiffer <Mattpfeiffer54@gmail.com>
2024-06-30 14:14:39 -0500

    Initial Commit

---
+++ FourthOfJuly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FourthOfJuly
+//
+//  Created by Matt Pfeiffer on 6/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FourthOfJuly/FourthOfJulyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FourthOfJulyApp.swift
+//  FourthOfJuly
+//
+//  Created by Matt Pfeiffer on 6/30/24.
+//
+
+import SwiftUI
+
+@main
+struct FourthOfJulyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
